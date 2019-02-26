5c22c9ed1cc2dd657a886382d55553df886c8a46
Jesus Ramirez <jesus.ramirez@sparktechs.com>
2019-02-25 16:10:31 -0800

    Deleted all files

--- generator.js
+++
@@ -1,7 +0,0 @@
-const { generatePosts, getPageandPosts } = require('./helper')
-
-function generator(allPosts, createPage) {
-  getPageandPosts(allPosts, createPage)
-}
-
-module.exports = { generator }
